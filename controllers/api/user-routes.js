039dc08d3d2b16194abd2ae6a2cf959799ecd356
HannahMidd <hmiddleton1017@gmail.com>
2023-07-06 16:01:04 -0400

    working on the user routes code

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,3 +1,70 @@
 const router = require('express').Router();
 const router = require('express').Router();
 const { User } = require('../../models');
+const router = require('express').Router();
+
+// Login session
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({
+            where: {
+                username: req.body.username,
+            },
+        });
+
+        if (!user) {
+            res.status(400).json({ message: 'Wrong password or username' });
+            return;
+        }
+
+        const validPassword = user.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Wrong password or username' });
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.userId = user.id;
+            req.session.username = user.username;
+            req.session.loggedIn = true;
+
+            res.json({ user, message: 'Logged in!' });
+        });
+    } catch (err) {
+        res.status(400).json({ message: 'Wrong password or username' });
+    }
+});
+
+// Logout session
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+//  Creating session
+router.post('/', async (req, res) => {
+    try {
+        const newUser = await User.create({
+            username: req.body.username,
+            password: req.body.password,
+        });
+
+        req.session.save(() => {
+            req.session.userId = newUser.id;
+            req.session.username = newUser.username;
+            req.session.loggedIn = true;
+
+            res.json(newUser);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
