97993f74e90e79b7e90d81c9a558091f8186c808
HannahMidd <hmiddleton1017@gmail.com>
2023-07-05 23:16:05 -0400

    adding more code to api routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -3,8 +3,52 @@ const { Post } = require('../../models/');
 const authenticate = require('../../utils/auth');
 const withAuth = require('../../utils/auth');
 
+// New post! Woohoo!
 router.post('/', authenticate, async, (req, res) => {
     const body = req.body;
-
-    // add trys, put and delete router
+      try {
+    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    res.json(newPost);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+//  Delete post 
+router.delete('/:id', authenticate, async (req, res) => {
+    try {
+        const [rowsImpacted]= Post.destroy({
+            where: {
+                id: req.params.id,
+            },
+        });
+        if (rowsImpacted > 0) {
+            res.status(200).end();
+        } else {
+            res.status(400).end();
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Update put request
+router.put('/:id', authenticate, async (req, res) => {
+  try {
+    const [rowsImpacted] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (rowsImpacted > 0) {
+      res.status(200).end();
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router; 
