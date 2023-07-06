f3ac7b72ada427df3c52e077df5287a30cc0dee8
HannahMidd <hmiddleton1017@gmail.com>
2023-07-06 16:42:44 -0400

    adding js code

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,168 @@
-// front-end logic goes here...
+// ============================ begin login section ============================
+
+const loginHandler = async function (event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input-login');
+    const passwordEl = document.querySelector('#password-input-login');
+
+    const response = await fetch('/api/user/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to login');
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', loginHandler);
+
+// ============================end login section ============================
+
+// ============================ begin signup section ============================
+const signupHandler = async function (event) {
+    event.preventDefault();
+
+    const usernameEl = document.querySelector('#username-input-signup');
+    const passwordEl = document.querySelector('#password-input-signup');
+
+    const response = await fetch('/api/user', {
+        method: 'POST',
+        body: JSON.stringify({
+            username: usernameEl.value,
+            password: passwordEl.value,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert('Failed to sign up');
+    }
+};
+
+document
+    .querySelector('#signup-form')
+    .addEventListener('submit', signupHandler);
+
+// ============================end signup section ============================
+
+// ============================begin post section ============================
+
+const newHandler = async function (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+
+    await fetch(`/api/post`, {
+        method: 'POST',
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    document.location.replace('/dashboard');
+};
+
+document.querySelector('#new-post-form').addEventListener('submit', newHandler);
+
+// ============================end post section ============================
+
+// ============================begin comment section ============================
+
+const commentHandler = async function (event) {
+    event.preventDefault();
+
+    const postId = document.querySelector('input[name="post-id"]').value;
+    const body = document.querySelector('textarea[name="comment-body"]').value;
+
+    if (body) {
+        await fetch('/api/comment', {
+            method: 'POST',
+            body: JSON.stringify({
+                postId,
+                body,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        document.location.reload();
+    }
+};
+
+document
+    .querySelector('#new-comment-form')
+    .addEventListener('submit', commentHandler);
+
+//  ============================end comment section ============================
+
+// ============================begin edit section ============================
+
+const postId = document.querySelector('input[name="post-id"]').value;
+
+const editHandler = async function (event) {
+    event.preventDefault();
+
+    const title = document.querySelector('input[name="post-title"]').value;
+    const body = document.querySelector('textarea[name="post-body"]').value;
+
+    await fetch(`/api/post/${postId}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    document.location.replace('/dashboard');
+};
+
+const deleteHandler = async function () {
+    await fetch(`/api/post/${postId}`, {
+        method: 'DELETE',
+    });
+
+    document.location.replace('/dashboard');
+};
+
+document
+    .querySelector('#edit-post-form')
+    .addEventListener('submit', editHandler);
+document.querySelector('#delete-btn').addEventListener('click', deleteHandler);
+
+// ============================end comment section ============================
+
+// ============================begin logout section ============================
+
+const logout = async function () {
+    const response = await fetch('/api/user/logout', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+        document.location.replace('/');
+    } else {
+        alert('Failed to log out');
+    }
+};
+
+document.querySelector('#logout-link').addEventListener('click', logout);
+
+// ============================end logout section ============================
